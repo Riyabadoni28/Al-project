c6b91665405c19501d13fe3f4af86b4406f391d6
riya badoini <riya.badoni@logicladder.com>
2026-09-03 11:57:54 +0530

    exceed size error fx

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://al-project-server.vercel.app',
+  apiUrl: 'http://localhost:8500/api',
 
   // Allow larger file uploads
   maxFileSize: 20 * 1024 * 1024, // 20MB in bytes
